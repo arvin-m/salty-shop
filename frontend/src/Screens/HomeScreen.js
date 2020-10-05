bedf96e04027423946b223ce56bca43265296b36
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2020-10-05 08:21:15 -0400

    crreating the root page of the app

---
+++ frontend/src/Screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import products from '../products';
+import { Row,Col, Card } from 'react-bootstrap';
+import Product from '../Components/Product';
+const HomeScreen = () => {
+    return (
+        <>
+            <h1>Latest products</h1>
+            <Row>
+                {products.map(product=>(
+                    <Col key={product._id} sm={12} md={6} lg={4}>
+                        <Product product={product} />
+                    </Col>
+                    
+                ))}
+            </Row>
+        </>
+    )
+}
+
+export default HomeScreen
---
+++ frontend/src/Screens/ProductScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import {Row, Col,Image,ListGroup,Cart,Button} from 'react-bootstrap'
+import Rating from '../Components/Rating'
+import products from '../products'
+const ProductScreen = () => {
+    return (
+        <div>
+            Product
+        </div>
+    )
+}
+
+export default ProductScreen
