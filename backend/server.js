550bb31e9354fae5a23a223269cc00c06528aa4e
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2020-10-10 22:38:22 -0400

    adding MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
-const express=require('express');
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
+import products from './data/products.js'
+dotenv.config();
+connectDB()
 const app=express();
-const products=require('./data/products')
 app.get('/',(req,res)=>{
     res.send('API is running...')
 })
@@ -12,5 +17,8 @@ app.get('/api/products/:id',(req,res)=>{
     res.json(product);
 })
 
-
-app.listen(5000,console.log('Server runing on port 5000'));
+const PORT=process.env.PORT || 5000
+app.listen(PORT,
+    console.log(`Server runing in ${process.env.NODE_ENV} mode on port ${PORT} . . .`.yellow.bold)
+    
+);
