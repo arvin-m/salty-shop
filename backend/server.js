a71fbaa3070983765e9eed9afa4585e2125a4922
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2020-10-12 12:55:55 -0400

    adding MiddleWare for Custome Err Handler

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import {notFound,errorHandler} from './middleware/errorMiddleWare.js'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 
@@ -11,7 +12,12 @@ const app=express();
 app.get('/',(req,res)=>{
     res.send('API is running...')
 })
+
 app.use('/api/products',productRoutes)
+
+// Custome Error MiddleWare
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT=process.env.PORT || 5000
 app.listen(PORT,
