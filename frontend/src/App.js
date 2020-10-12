eec0b4cfc396eeae4f0678ee33b5713c009e01c2
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2020-10-12 19:43:05 -0400

    adding Redux and create Global state for Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import {Container} from 'react-bootstrap'
+
 import Header from './Components/Header'
 import Footer from './Components/Footer'
 import HomeScreen from './Screens/HomeScreen'
--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,34 +1,38 @@
-import React, {useState, useEffect} from 'react'
-import { Row,Col, Card } from 'react-bootstrap'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, Card } from 'react-bootstrap'
 import Product from '../Components/Product'
-import axios from 'axios'
+import { listProducts } from '../actions/productActions'
+
 const HomeScreen = () => {
 
-    const[products,setProducts]=useState([]);
-    
+    const dispatch = useDispatch()
+    const productList = useSelector(state => state.productList)
+    const { loading, error, products } = productList
 
-    useEffect(()=>{
-        const fetchProducts=async () =>{
-            const {data} = await axios.get('/api/products')
-            console.log(data)
-            setProducts(data)
-        }
-        
-        fetchProducts()
-    },[])
-    console.log("PRODUCTS ",products)
+
+    useEffect(() => {
+        dispatch(listProducts())
+
+    }, [dispatch])
+
+
 
     return (
         <>
             <h1>Latest products</h1>
-            <Row>
-                {products.map(product=>(
+            {loading ?( <h2>Loading ...</h2>) : error ?( <h3>{error}</h3> ):(
+
+                <Row>
+                {products.map(product => (
                     <Col key={product._id} sm={12} md={6} lg={4}>
                         <Product product={product} />
                     </Col>
-                    
                 ))}
             </Row>
+
+              )}
+           
         </>
     )
 }
