705f5c683be1e6bec47664bc125509a776ebf4f6
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2020-10-14 23:09:59 -0400

    creating global state for cart and adding functionality to add and remove to the cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import {Container} from 'react-bootstrap'
-
 import Header from './Components/Header'
 import Footer from './Components/Footer'
 import HomeScreen from './Screens/HomeScreen'
 import ProductScreen from './Screens/ProductScreen'
-
+import CartScreen from './Screens/CartScreen'
 
 
 const App = () => {
@@ -17,6 +16,7 @@ const App = () => {
         <Container>
           <Route path='/' component={HomeScreen} exact/>
           <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />
         </Container>
       </main>
       <Footer/>
--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap'
 import Rating from '../Components/Rating'
 import { listProductDetails } from '../actions/productActions'
 import Loader from '../Components/Loader'
 import Message from '../Components/Message'
 
-const ProductScreen = ({ match }) => {
+const ProductScreen = ({ history,match }) => {
+
+    const [qty,setQty]=useState(1)
+
+
     const dispatch = useDispatch()
     const productDetails = useSelector(state => state.productDetails)
     const { loading, error, product } = productDetails
@@ -15,6 +19,11 @@ const ProductScreen = ({ match }) => {
     useEffect(() => {
         dispatch(listProductDetails(match.params.id))
     }, [dispatch, match])
+
+    const addToCartHandler=(e)=>{
+        history.push(`/cart/${match.params.id}?qty=${qty}`)
+
+    }
 
     return (
         <>
@@ -51,8 +60,24 @@ const ProductScreen = ({ match }) => {
                                         <Col>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</Col>
                                     </Row>
                                 </ListGroup.Item>
+                                {product.countInStock >0 && (
+                                    <ListGroup.Item>
+                                        <Row>
+                                            <Col>Qty</Col>
+                                            <Col>
+                                            <Form.Control as='select' value={qty} onChange={(e)=> setQty(e.target.value)}>
+                                                {[...Array(product.countInStock).keys()].map(x=>(
+                                                    <option key={x} value={x+1}>
+                                                        {x+1}
+                                                    </option>
+                                                ))}
+                                            </Form.Control>
+                                            </Col>
+                                        </Row>
+                                    </ListGroup.Item>
+                                )}
                                 <ListGroup.Item>
-                                    <Button className='btn-block ' type='button' disabled={product.countInStock === 0} >Add To Cart</Button>
+                                    <Button onClick={addToCartHandler} className='btn-block ' type='button' disabled={product.countInStock === 0} >Add To Cart</Button>
                                 </ListGroup.Item>
 
                             </ListGroup>
