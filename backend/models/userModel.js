83eedf13e0cbea32ece5fe35d31f92b39800c4aa
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2020-10-22 22:56:18 -0400

    creating controller&route for new user registration,add method for hash the password to the user Schima

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -29,5 +29,20 @@ userSchema.methods.matchPassword = async function(enteredPassword){
 
 }
 
+// Hash the password before createing new user in datatbase
+userSchema.pre('save',async function(next){
+    // check for if user password is already there ,when u wanna update the user info we dont wanna hash the pass again
+    if(!this.isModified('password')){
+        next()
+    }
+
+
+    const salt = await bcrypt.genSalt(10)
+    this.password= await bcrypt.hash(this.password,salt)
+
+
+
+})
+
 const User = mongoose.model('User', userSchema)
 export default User
