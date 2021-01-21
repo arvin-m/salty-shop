dde35361e97acb21679f0e5ca21d4b27d5f51cb4
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2021-01-21 17:42:07 -0500

    creating route and controller for product review + add {user} to product schema

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -71,7 +71,7 @@ const createProduct = asyncHandler(async (req, res) => {
 // @route PUt/api/products/:id
 // @access Private/Admin
 const updateProduct = asyncHandler(async (req, res) => {
-    const {name,price,image,brand,category,countInStock,numReviews,description}=req.body
+    const {name,price,image,brand,category,countInStock,description}=req.body
 
     const product= await Product.findById(req.params.id)
 
@@ -92,9 +92,48 @@ const updateProduct = asyncHandler(async (req, res) => {
         throw new Error('Product not found!')
     }
 
-    
-  
 
 })
 
-export { getProducts, getProductById, deleteProduct,createProduct,updateProduct }
+// @desc Create New Review
+// @route POST/api/products/:id/reviews
+// @access Private
+const createProductReview = asyncHandler(async (req, res) => {
+    const { rating, comment } = req.body
+  
+    const product = await Product.findById(req.params.id)
+  
+    if (product) {
+      const alreadyReviewed = product.reviews.find(
+        (r) => r.user.toString() === req.user._id.toString()
+      )
+  
+      if (alreadyReviewed) {
+        res.status(400)
+        throw new Error('Product already reviewed')
+      }
+  
+      const review = {
+        name: req.user.name,
+        rating: Number(rating),
+        comment,
+        user: req.user._id,
+      }
+  
+      product.reviews.push(review)
+  
+      product.numReviews = product.reviews.length
+  
+      product.rating =
+        product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+        product.reviews.length
+  
+      await product.save()
+      res.status(201).json({ message: 'Review added' })
+    } else {
+      res.status(404)
+      throw new Error('Product not found')
+    }
+  })
+
+export { getProducts, getProductById, deleteProduct,createProduct,updateProduct, createProductReview }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express'
-import { getProducts, getProductById, deleteProduct,createProduct, updateProduct } from '../controller/productController.js'
+import { getProducts, getProductById, deleteProduct,createProduct, updateProduct, createProductReview } from '../controller/productController.js'
 import { protect, admin } from '../middleware/authMiddleWare.js'
 
 const router = express.Router()
 
 
 router.route('/').get(getProducts).post(protect,admin,createProduct)
+router.route('/:id/reviews').post(protect,createProductReview)
+
 router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct).put(protect,admin,updateProduct)
 
 export default router
