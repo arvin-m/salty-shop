2caa99c1038ad75e093fa819d39fa108ecfd0404
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2021-01-23 15:08:54 -0500

    clean userEditScreen

--- frontend/src/Screens/UserListScreen.js
+++ frontend/src/Screens/UserListScreen.js
@@ -4,45 +4,37 @@ import { Table, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../Components/Message'
 import Loader from '../Components/Loader'
-import { listUsers,deleteUser } from '../actions/userActions'
+import { listUsers, deleteUser } from '../actions/userActions'
 
-const UserListScreen = ({history}) => {
+const UserListScreen = ({ history }) => {
+  const dispatch = useDispatch()
 
-    const dispatch = useDispatch()
- 
-    const userList = useSelector((state) => state.userList)
-    const { loading, error, users } = userList
-    console.log("userList-->> ",userList)
+  const userList = useSelector((state) => state.userList)
+  const { loading, error, users } = userList
 
-    const userLogin = useSelector((state) => state.userLogin)
-    const {userInfo } = userLogin
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
 
-    const userDelete = useSelector((state) => state.userDelete)
-    const {success:successDelete } = userDelete
+  const userDelete = useSelector((state) => state.userDelete)
+  const { success: successDelete } = userDelete
 
-    useEffect(() => {
-        if(userInfo && userInfo.isAdmin){
-            dispatch(listUsers())
-        }else{
-            history.push('/login')
-        }
-
-    }, [dispatch, history ,successDelete, userInfo])
-
-
-    const deleteHandler = (id) => {
-        if(window.confirm('Are you sure')){
-            dispatch(deleteUser(id))
-
-        }
-
+  useEffect(() => {
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listUsers())
+    } else {
+      history.push('/login')
     }
+  }, [dispatch, history, successDelete, userInfo])
 
+  const deleteHandler = (id) => {
+    if (window.confirm('Are you sure')) {
+      dispatch(deleteUser(id))
+    }
+  }
 
-
-    return (
-        <>
-           <h1>Users</h1>
+  return (
+    <>
+      <h1>Users</h1>
       {loading ? (
         <Loader />
       ) : error ? (
@@ -91,10 +83,9 @@ const UserListScreen = ({history}) => {
             ))}
           </tbody>
         </Table>
-      )
-      }
-        </>
-    )
+      )}
+    </>
+  )
 }
 
 export default UserListScreen
