c3ec337020ee0141bee72e8712dc68e06fc9860e
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2021-01-23 16:33:57 -0500

    prepare for production and create a build folder

---
+++ frontend/build/precache-manifest.e9c1c0e9ea894da9edc2596574d13a26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b2943017a869e15bad79a878a5f1bcf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6b5fa23349cea3dce9d",
+    "url": "/static/css/main.3742678f.chunk.css"
+  },
+  {
+    "revision": "ce87eddbeed325bb663b",
+    "url": "/static/js/2.4e246636.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4e246636.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6b5fa23349cea3dce9d",
+    "url": "/static/js/main.f18102c4.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
