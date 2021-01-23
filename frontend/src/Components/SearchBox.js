d9582adffff7aa0e8f849491a304b81f56c2c699
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2021-01-23 13:58:26 -0500

    deleting unused photo and add new photo for new product

--- frontend/src/Components/SearchBox.js
+++ frontend/src/Components/SearchBox.js
@@ -20,7 +20,7 @@ const SearchBox = ({history}) => {
         <Form onSubmit={submitHandler} inline>
             <Form.Control type='text' name='q' onChange={(e)=> setKeyword(e.target.value)}
              placeholder='Search Products...' className='mr-sm-2 ml-sm-5'></Form.Control>
-             <Button type='submit' variant='outline-success' className='p-2'>Search</Button>
+             <Button type='submit' variant='outline-secondary'size="sm"  >Search</Button>
             
         </Form>
     )
