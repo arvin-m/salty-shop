27e56dae3d2a301ecd0be2479c1664f2fa2318dd
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2021-01-23 16:35:31 -0500

    create static folder for production

--- backend/server.js
+++ backend/server.js
@@ -23,9 +23,7 @@ if(process.env.NODE_ENV === 'development'){
 //allow us to get the json request from the body
 app.use(express.json())
 
-app.get('/',(req,res)=>{
-    res.send('API is running...')
-})
+
 
 app.use('/api/products',productRoutes)
 app.use('/api/users',userRoutes)
@@ -39,6 +37,15 @@ app.get('/api/config/paypal',(req,res)=>res.send(process.env.PAYPAL_CLIENT_ID))
 const __dirname=path.resolve()
 app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
 
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'/frontend/build')))
+    app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'frontend','build','index.html')))
+}else{
+    app.get('/',(req,res)=>{
+        res.send('API is running...')
+    })
+}
 
 // Custome Error MiddleWare
 app.use(notFound)
