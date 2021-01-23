e0992c5840b427dc9d5ec67f4e8568e0be19de49
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2021-01-23 15:04:33 -0500

    FIxing typo on app.js for product Edit screen

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -87,22 +87,20 @@ export const productCreateReducer = (state = {}, action) => {
 
 }
 
-export const productUpdateReducer = (state = {product:{}}, action) => {
+export const productUpdateReducer = (state = { product: {} }, action) => {
     switch (action.type) {
-        case PRODUCT_UPDATE_REQUEST:
-            return { loadig: true }
-        case PRODUCT_UPDATE_SUCCESS:
-            return { loading: false, success: true , product:action.payload}
-        case PRODUCT_UPDATE_FAIL:
-            return { loading: false, error: action.payload }
-        case PRODUCT_UPDATE_RESET:
-            return { product: {} }
-        default:
-            return state
-
+      case PRODUCT_UPDATE_REQUEST:
+        return { loading: true }
+      case PRODUCT_UPDATE_SUCCESS:
+        return { loading: false, success: true, product: action.payload }
+      case PRODUCT_UPDATE_FAIL:
+        return { loading: false, error: action.payload }
+      case PRODUCT_UPDATE_RESET:
+        return { product: {} }
+      default:
+        return state
     }
-
-}
+  }
 
 export const productReviewCreateReducer = (state = {}, action) => {
     switch (action.type) {
